15f22d82c5ef26b59552d1eadd0baa5f74275f2c
bala-amypo <bala@amypo.in>
2025-12-21 13:54:09 +0000

    User commit

--- src/main/java/com/example/demo/config/repository/DynamicPriceRecordRepository.java
+++ src/main/java/com/example/demo/config/repository/DynamicPriceRecordRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface DynamicPriceRecordRepository extends JpaRepository<DynamicPriceRecord, Long> {
+public interface DynamicPriceRecordRepository
+        extends JpaRepository<DynamicPriceRecord, Long> {
 
     List<DynamicPriceRecord> findByEventIdOrderByComputedAtDesc(Long eventId);
 
