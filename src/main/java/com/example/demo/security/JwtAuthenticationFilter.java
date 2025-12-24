f8b492595d82013048537b01de4dd71e4c7c3e6d
bala-amypo <bala@amypo.in>
2025-12-24 05:52:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
@@ -0,0 +1,52 @@
+package com.example.demo.security;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+ private final JwtTokenProvider tokenProvider;
+
+ public JwtAuthenticationFilter(JwtTokenProvider tokenProvider) {
+  this.tokenProvider = tokenProvider;
+ }
+
+ @Override
+ protected void doFilterInternal(HttpServletRequest request,
+                                 HttpServletResponse response,
+                                 FilterChain filterChain)
+         throws ServletException, IOException {
+
+  String header = request.getHeader("Authorization");
+
+  if (header != null && header.startsWith("Bearer ")) {
+   String token = header.substring(7);
+
+   if (tokenProvider.validateToken(token)) {
+    Claims claims = tokenProvider.getClaimsFromToken(token);
+
+    UsernamePasswordAuthenticationToken auth =
+            new UsernamePasswordAuthenticationToken(
+                    claims.get("email"),
+                    null,
+                    Collections.emptyList()
+            );
+
+    SecurityContextHolder.getContext().setAuthentication(auth);
+   }
+  }
+
+  filterChain.doFilter(request, response);
+ }
+}
